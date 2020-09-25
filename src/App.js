f9ac735a870cacb9718de584309fec696c61e82d
seolin <iseolin76@gmail.com>
2020-09-25 15:28:39 +0900

    Add foodLike

--- src/App.js
+++ src/App.js
@@ -4,14 +4,31 @@ function Food({fav}){
   return <h1>I like {fav}</h1>
 }
 
+const foodLike = [
+  {
+    name: "kimchi",
+    image: "kimchi"
+  },
+  {
+    name: "ramen",
+    image: "ramen"
+  },
+  {
+    name: "samgionsal",
+    image: "samgionsal"
+  },
+  {
+    name: "dnddj",
+    image: "dnddj"
+  }
+]
+
 function App() {
   return (
     <div>
       <div>Hello</div>
-      <Food fav="kimchi"/>
-      <Food fav="ramen"/>
-      <Food fav="samgionsal"/>
-      <Food fav="dndjd"/>
+      {foodLike.map(dish => 
+      <food name={dish.name} />)}
     </div>
   );
 }
