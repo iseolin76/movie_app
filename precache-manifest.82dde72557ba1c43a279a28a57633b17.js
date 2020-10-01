e6635a92ffdf74dd40e79ee22e65f9f22e388d03
seolin <iseolin76@gmail.com>
2020-10-01 23:40:12 +0900

    Updates

---
+++ precache-manifest.82dde72557ba1c43a279a28a57633b17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ac222d990274d8dba8c5eb54361135",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "eb0c5b096da1563c590b",
+    "url": "/movie_app/static/css/main.6c62520f.chunk.css"
+  },
+  {
+    "revision": "9287b62ddf780fa0a1a5",
+    "url": "/movie_app/static/js/2.de0afe8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.de0afe8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb0c5b096da1563c590b",
+    "url": "/movie_app/static/js/main.b408e6b0.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
