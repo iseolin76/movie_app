f79b2c070880f305963a9048314952d6639b3654
seolin <iseolin76@gmail.com>
2020-10-02 21:31:49 +0900

    Fix err

--- src/Movie.js
+++ src/Movie.js
@@ -1,21 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./App.css";
+import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres }) {
-    return <div className="movie">
-        <img src={poster} alt={title} title={title} />
-        <div className="movie__data">
-            <h3 className="movie__title">{title}</h3>
-            <h5 className="movie__year">{year}</h5>
-            <ul className="genres">
-                {genres.map(genres, index => (
-                    <li key={index} className="genres__genre">{genres}</li>
-                ))}
-            </ul>
-            <p className="movie__summary">{summary.slice(0, 180)}...</p>
+    return (
+        <div className="movie">
+            <img src={poster} alt={title} title={title} />
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="genres">
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres__genre">
+                            {genre}
+                        </li>
+                    ))}
+                </ul>
+                <p className="movie__summary">{summary.slice(0, 180)}...</p>
+            </div>
         </div>
-    </div>
+    );
 }
 
 Movie.propTypes = {
@@ -25,6 +29,6 @@ Movie.propTypes = {
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
     genres: PropTypes.arrayOf(PropTypes.string).isRequired
-}
+};
 
 export default Movie;
